b3676114b7b3807aefc6069c6a415edace8e8d11
lsti1727375 <josejulio2119@gmail.com>
2017-04-23 22:26:29 -0500

    factory

---
+++ FactoryPetternDemo/src/factorypetterndemo/ShapeFactory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorypetterndemo;
+
+/**
+ *
+ * @author moust
+ */
+public class ShapeFactory {
+    public Shape getShape(String shapeType){
+      if(shapeType == null){
+         return null;
+      }
+      if(shapeType.equals("CIRCLE")){
+         return new Circle();
+
+      } else if(shapeType.equals("RECTANGLE")){
+         return new Rectangle();
+
+      } else if(shapeType.equals("SQUARE")){
+         return new Square();
+      }
+
+      return null;
+   }
+}
